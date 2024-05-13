05edfaa387526bd27e5939868f57d76603582af9
YimingIsCOLD <yimingpeh@gmail.com>
2024-05-13 16:08:18 +0800

    feat: integrate with `@astrojs/node` in standalone mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
+import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  outDir: 'build',
+  output: 'hybrid',
+  build: {
+    client: './build/client',
+    server: './build/server',
+  },
+  adapter: node({
+    mode: 'standalone',
+  }),
+
   integrations: [
     tailwind({
       applyBaseStyles: false,
