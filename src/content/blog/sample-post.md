bb91f134cb9e05f7cfd350d9bd9d050a23bea897
Alex Ng <7019953+axxng@users.noreply.github.com>
2024-11-19 11:26:54 +0800

    feat: read markdown files and render on /blog

---
+++ src/content/blog/sample-post.md
@@ -0,0 +1,16 @@
+---
+title: 'My Sample Blog Post'
+description: 'This is my sample blog post using Astro'
+pubDate: 2024-01-01
+author: 'Your Name'
+---
+
+# Welcome to my blog
+
+This is my first blog post! You can use **Markdown** here.
+
+## Features
+
+- Easy to write
+- Supports all Markdown features
+- Automatically generates pages
---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import { defineCollection, z } from 'astro:content';
+
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    author: z.string().optional(),
+    image: z.string().optional(),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
