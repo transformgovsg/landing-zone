0e5cbf5ab301e97a04969795ec423ff6edfa1087
Alex Ng <7019953+axxng@users.noreply.github.com>
2024-11-20 13:53:41 +0800

    chore: read blog posts from local directory when in development

--- src/services/base-blog.service.ts
+++ src/services/base-blog.service.ts
@@ -2,18 +2,33 @@
 
 import { parse as parseYaml } from 'yaml';
 import { fetchGithubContent } from '../utils/github';
+import fs from 'node:fs/promises';
+import path from 'node:path';
 
 export abstract class BaseBlogService {
-  //   protected static readonly BLOG_API_URL = 'https://api.github.com/repos/transformgovsg/landing-zone/contents/src/content/blog';
+  // protected static readonly BLOG_API_URL = 'https://api.github.com/repos/transformgovsg/landing-zone/contents/src/content/blog';
   protected static readonly BLOG_API_URL =
     'https://api.github.com/repos/transformgovsg/landing-zone/contents/src/content/blog?ref=feat/add-blogging-capabilities';
+  protected static readonly LOCAL_BLOG_PATH = 'src/content/blog';
 
   protected static async fetchBlogFiles() {
+    if (import.meta.env.DEV) {
+      const files = await fs.readdir(this.LOCAL_BLOG_PATH);
+      return files.map((name) => ({
+        name,
+        download_url: path.join(this.LOCAL_BLOG_PATH, name),
+      }));
+    }
+
     const response = await fetchGithubContent(this.BLOG_API_URL);
     return response.json();
   }
 
   protected static async fetchFileContent(downloadUrl: string): Promise<string> {
+    if (import.meta.env.DEV) {
+      return fs.readFile(downloadUrl, 'utf-8');
+    }
+
     return fetchGithubContent(downloadUrl).then((res) => res.text());
   }
 
