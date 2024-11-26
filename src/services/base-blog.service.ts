208b493b6b4f56e3965b40f4874c2b47865ffabd
Alex Ng <7019953+axxng@users.noreply.github.com>
2024-11-26 16:16:42 +0800

    chore: move BLOG_API_URL to env var to allow targeting of different branches for preview purposes

--- src/services/base-blog.service.ts
+++ src/services/base-blog.service.ts
@@ -6,8 +6,7 @@ import fs from 'node:fs/promises';
 import path from 'node:path';
 
 export abstract class BaseBlogService {
-  protected static readonly BLOG_API_URL =
-    'https://api.github.com/repos/transformgovsg/landing-zone/contents/src/content/blog';
+  protected static readonly BLOG_API_URL = import.meta.env.BLOG_API_URL;
   protected static readonly LOCAL_BLOG_PATH = 'src/content/blog';
 
   protected static async fetchBlogFiles() {
